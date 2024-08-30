1e418932c665e490ab3bf7ca40c875dde51d7921
AugustasPauzas <augustaspauzas@gmail.com>
2024-08-30 14:32:44 +0300

    update 2

--- resources/views/phpinfo.blade.php
+++ resources/views/phpinfo.blade.php
@@ -12,6 +12,7 @@
         <!-- Styles -->
 
     </head>
+    <p> commit 2 test </p>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
