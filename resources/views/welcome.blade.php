f2df5b47e9551b6a979254114bf19a60bf63c7d7
AugustasPauzas <augustaspauzas@gmail.com>
2024-08-30 15:29:49 +0300

    update 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel Test</title>
 
+        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LeMjjIqAAAAAG-l7cKzzWgLxVfcB0QC0tM7q9_q"></script>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -22,6 +24,16 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <p>TEST 2</p>
+
+                    <script>
+                        function onClick(e) {
+                          e.preventDefault();
+                          grecaptcha.enterprise.ready(async () => {
+                            const token = await grecaptcha.enterprise.execute('6LeMjjIqAAAAAG-l7cKzzWgLxVfcB0QC0tM7q9_q', {action: 'LOGIN'});
+                          });
+                        }
+                    </script>
+
                     
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
