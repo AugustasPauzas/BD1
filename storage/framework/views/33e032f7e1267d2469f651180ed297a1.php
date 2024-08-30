a476d691e94bafd19c07059afe8b20d2cd2d9b22
AugustasPauzas <augustaspauzas@gmail.com>
2024-08-30 14:26:35 +0300

    first commit

---
+++ storage/framework/views/33e032f7e1267d2469f651180ed297a1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <!-- Styles -->
+
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            
+                    <?php
+                    phpinfo();
+                    ?>
+
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php /**PATH C:\Users\admin\Desktop\lar\resources\views/phpinfo.blade.php ENDPATH**/ ?>
